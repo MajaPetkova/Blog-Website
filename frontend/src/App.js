8edcfb4fa001815ac53ee900826207aeb3bf2a3c
Maja Petkova <majapetkova1@yahoo.com>
2024-08-08 12:22:49 +0200

    Add routing for pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,13 +5,25 @@ import Write from "./pages/write/Write";
 import Settings from "./pages/settings/Settings";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
+  const user = true;
   return (
     <>
       <Topbar />
-      {/* <Login/> */}
-      <Register/>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/login" element={user ? <Home /> : <Login />} />
+
+        <Route path="/register" element={user ? <Home /> : <Register />} />
+
+        <Route path="/write" element={user ? <Write />:<Register/>} />
+        <Route path="/post/:id" element={<Single />} />
+        <Route path="/settings" element={user ?<Settings /> :<Register/>} />
+        <Route />
+        <Route />
+      </Routes>
     </>
   );
 }
