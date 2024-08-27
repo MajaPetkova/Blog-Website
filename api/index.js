2be054fda51dccd0a3aabb1cd52a77bd27ab9402
Maja Petkova <majapetkova1@yahoo.com>
2024-08-27 19:06:45 +0200

    Add bcrypt library for hashing password, add register route

--- api/index.js
+++ api/index.js
@@ -1,19 +1,22 @@
 const express = require("express");
-const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
+
+const app = express();
+app.use(express.json());
 
 dotenv.config();
+
 mongoose
-  .connect(
-    process.env.MONGO_URL,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
+
+app.use("/api/auth", authRoute);
 
 app.listen("5000", () => {
   console.log("App is listening on port 5000");
