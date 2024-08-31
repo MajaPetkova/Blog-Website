5570bdc7b463ccf7afe0eba80f70e1d6926fefa7
Maja Petkova <majapetkova1@yahoo.com>
2024-08-31 18:49:28 +0200

    Fetching single post using axios

--- frontend/src/components/singlePost/SinglePost.jsx
+++ frontend/src/components/singlePost/SinglePost.jsx
@@ -1,16 +1,33 @@
+import { useLocation } from "react-router-dom";
 import "./singlePost.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const SinglePost = () => {
+  const location = useLocation();
+  const path = location.pathname.split("/")[2];
+  const [post, setPost] = useState({});
+
+
+  useEffect(() => {
+    const getPost = async () => {
+      const res = await axios.get("/posts/" + path);
+      setPost(res.data);
+    };
+    getPost();
+  }, [path]);
+
   return (
     <div className="singlePost">
       <div className="singlePostWrapper">
+        {post.photo && 
         <img
           className="singlePostImg"
-          src="https://images.pexels.com/photos/259620/pexels-photo-259620.jpeg?auto=compress&cs=tinysrgb&w=600"
+          src={post.photo}
           alt=""
-        />
+        />}
         <h1 className="singlePostTitle">
-          Lorem ipsum dolor sit amet
+          {post.title}
           <div className="singlePostEdit">
             <i className="singlePostIcon fa-regular fa-pen-to-square"></i>
             <i className="singlePostIcon fa-solid fa-trash"></i>
@@ -18,34 +35,12 @@ const SinglePost = () => {
         </h1>
         <div className="singlePostInfo">
           <span className="singlePostAuthor">
-            Author: <b>Marija</b>
+            Author: <b>{post.username}</b>
           </span>
-          <span className="singlePostDate">1 hour ago</span>
+          <span className="singlePostDate">{new Date(post.updatedAt).toDateString()}</span>
         </div>
         <p className="singlePostDesc">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos
-          asperiores commodi quo iure ratione atque! Ullam eum cum consectetur
-          quod? Veniam perferendis maxime consequuntur dignissimos nesciunt
-          minus incidunt inventore. Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Libero distinctio cumque necessitatibus et dolor
-          aperiam deleniti velit amet delectus dolorem!Veniam perferendis maxime
-          consequuntur dignissimos nesciunt minus incidunt inventore. Lorem
-          ipsum dolor sit amet consectetur adipisicing elit. Libero distinctio
-          cumque necessitatibus et dolor aperiam deleniti velit amet delectus
-          dolorem! Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-          Consequuntur nulla ad commodi ut eius blanditiis placeat, maxime animi
-          maiores optio quos qui. Quod, minima maxime voluptatum quis molestiae
-          at. Qui illum odio fugiat, accusamus nam a id deserunt, cum animi ex
-          ratione tenetur aspernatur possimus officia non atque debitis
-          deleniti, molestiae quae quas rem. Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Libero distinctio cumque necessitatibus
-          et dolor aperiam deleniti velit amet delectus dolorem!Veniam
-          perferendis maxime consequuntur dignissimos nesciunt minus incidunt
-          inventore. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Libero distinctio cumque necessitatibus et dolor aperiam deleniti
-          velit amet delectus dolorem! Lorem, ipsum dolor sit amet consectetur
-          adipisicing elit. Consequuntur nulla ad commodi ut eius blanditiis
-          placeat, maxime animi maiores optio quos qui.
+         {post.desc}
         </p>
       </div>
     </div>
