6369f88905f5420a0803721d041369ba1d31f232
Maja Petkova <majapetkova1@yahoo.com>
2024-09-04 17:40:42 +0200

    Add handleDelete for the own post

--- frontend/src/components/singlePost/SinglePost.jsx
+++ frontend/src/components/singlePost/SinglePost.jsx
@@ -1,13 +1,15 @@
 import { Link, useLocation } from "react-router-dom";
 import "./singlePost.css";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import axios from "axios";
+import { Context } from "../../context/Context";
 
 const SinglePost = () => {
   const PF = "http://localhost:5000/images/";
   const location = useLocation();
   const path = location.pathname.split("/")[2];
   const [post, setPost] = useState({});
+  const {user} = useContext(Context)
 
   useEffect(() => {
     const getPost = async () => {
@@ -17,6 +19,16 @@ const SinglePost = () => {
     getPost();
   }, [path]);
 
+
+  const handleDelete =async() =>{
+    try{
+     await axios.delete(`/posts/${post._id}`, {data:{username: user.username}})
+      window.location.replace("/")
+    }catch(err){
+
+    }
+  }
+
   return (
     <div className="singlePost">
       <div className="singlePostWrapper">
@@ -24,11 +36,13 @@ const SinglePost = () => {
           <img className="singlePostImg" src={PF + post.photo} alt="" />
         )}
         <h1 className="singlePostTitle">
-          {post.title}
+          {post.title }
+          {post.username === user?.username && 
           <div className="singlePostEdit">
             <i className="singlePostIcon fa-regular fa-pen-to-square"></i>
-            <i className="singlePostIcon fa-solid fa-trash"></i>
+            <i className="singlePostIcon fa-solid fa-trash" onClick={handleDelete}></i>
           </div>
+          }
         </h1>
         <div className="singlePostInfo">
           <span className="singlePostAuthor">
